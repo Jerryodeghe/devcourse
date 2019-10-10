f977024a5082c4d5c3ae5c63d1c7b2d6a532fa15
Jerry Odeghe <odeghejerry@gmail.com>
2019-10-10 11:17:59 +0100

    precalculator exercise solution

--- project1guide/app.js
+++ project1guide/app.js
@@ -18,17 +18,67 @@
 // change the display message to be green
 
 
+// EXERCISE 4
+// Remove the old state of sales summary
+
+// Solution to exercise 1
+function isValidUserInput(values) {
+    let isValid = false;
+    const { name, price, qty } = values;
+    if ( name === '' || price === '' || qty === '') {
+        return isValid; 
+    }
+    //if( (typeof Number(price) !== 'number') || (typeof Number(qty) !== 'number') )
+    if ( isNaN(price) || isNaN(qty) ) {
+        return isValid;
+    } 
+    if ( (price > 500) || (price < 1) ) {
+        return isValid;
+    }
+    
+    if ( (qty > 25 || qty < 1) ) {
+        return isValid;
+    }
+    isValid = true;
+    return isValid;
+}
+
+// Solution to exercise 2 and 3
+function changeOuputColor() {
+    document.getElementById('total-qty').style.color = 'grey';
+    document.getElementById('total-price').style.color = 'grey';
+    document.getElementById('total').style.color = 'blue';
+    document.getElementById('res').style.color = 'green';
+}
+
+// Solution to exercise 4
+// Removes the old result of displayOldSummary
+function clearOldResults() {
+    const res = document.getElementById('res');
+    const child = res.firstElementChild;
+    if (child !== null) {
+        res.removeChild(child);        
+    }
+}
+
 function calculate() {
     const values = getInputValue();
-    const name = values.name;
-    const price = values.price;
-    const qty = values.qty;
-    const total = calculateTotal(price, qty);
-    const salesObj = createSalesObject(name, price, qty, total);
-    const formattedSalesInfo = formatSalesInfo(salesObj);
-    outputCaluclatedUserInput(qty, price, total);
-    displaySalesSummary(formattedSalesInfo);
-    clearInputValue();
+    clearOldResults();  
+
+    if ( isValidUserInput(values) ) {
+        const name = values.name;
+        const price = values.price;
+        const qty = values.qty;
+        const total = calculateTotal(price, qty);
+        const salesObj = createSalesObject(name, price, qty, total);
+        const formattedSalesInfo = formatSalesInfo(salesObj);
+        changeOuputColor();
+        outputCaluclatedUserInput(qty, price, total);
+        displaySalesSummary(formattedSalesInfo);
+        clearInputValue();
+    } else {
+        alert('Invalid inputs');
+    } 
 }
 
 function calculateTotal(quantity, price) {
